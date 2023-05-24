298971d7a587a2cdf395bdee2ecba3c557f7e2e6
chomosuke <a13323600@gmail.com>
2023-05-25 04:32:53 +1000

    4 space indent like any non-psychopath

--- stringtointeger.ads
+++ stringtointeger.ads
@@ -2,7 +2,7 @@
 
 package StringToInteger with SPARK_Mode Is
    
-   -- returns 0 in case of invalid input
-   function From_String(S : in String) return Integer;
+    -- returns 0 in case of invalid input
+    function From_String(S : in String) return Integer;
    
 end StringToInteger;
