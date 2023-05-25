a413f20e217b64f8587e1f6da4c46020fe41eb5e
Michael Manoussakis <mihalis.mana@gmail.com>
2023-05-25 17:46:09 +1000

    Explain mystringtokeniser.ads Tokenise post-condition

--- mystringtokeniser.ads
+++ mystringtokeniser.ads
@@ -13,13 +13,35 @@ package MyStringTokeniser with SPARK_Mode is
       (Ch = ' ' or Ch = Ada.Characters.Latin_1.LF or
          Ch = Ada.Characters.Latin_1.HT);
 
-    procedure Tokenise(S : in String; Tokens : in out TokenArray; Count : out Natural) with
-      Pre => (if S'Length > 0 then S'First <= S'Last) and Tokens'First <= Tokens'Last,
-      Post => Count <= Tokens'Length and
-      (for all Index in Tokens'First..Tokens'First+(Count-1) =>
-           (Tokens(Index).Start >= S'First and
-                  Tokens(Index).Length > 0) and then
-             Tokens(Index).Length-1 <= S'Last - Tokens(Index).Start);
 
+    -- This procecure takes an immutable String S,
+    --  mutates a TokenArray Tokens, and
+    --  returns a Natural number Count
+    procedure Tokenise(S: in String; Tokens: in out TokenArray; Count: out Natural) with
+      -- Ensure that if the string is not empty, then the position of its first element
+      --  is at least as great as the position of the last element.
+      --  Ensure that Tokens has a positive length with valid indices.
+      Pre => (if S'Length > 0 then S'First <= S'Last) and Tokens'First <= Tokens'Last,
+
+      -- Ensure that our output, Count, has a value of at most the post-mutation length of Tokens
+      --  This allows callers to use Count as the right-bound of a range when indexing TokenArray
+      --  without fear of out-of-bounds errors.
+      Post => Count <= Tokens'Length and
+      -- For each element (token) in tokens, we perform several checks to ensure that the bounds
+      --  of the token indices are within the bounds of the string indices, with the following
+      --  checks.
+      -- This allows callers to use Tokens(Index).Start as the left-bound of a range when indexing
+      --  TokenArray, and use (Tokens(Index).Start .. (Tokens(Index).Start + Tokens(Index).Length))
+      --  as the range when indexing within a specific token (that is, an element of Tokens),
+      --  without fear of out-of-bounds errors.
+      (for all Index in Tokens'First..Tokens'First+(Count-1) =>
+           -- Ensure that the first index of each token is no less than
+           --  the index of the first character of the string.
+           (Tokens(Index).Start >= S'First and
+                  -- Ensure that the token is non-empty.
+                  Tokens(Index).Length > 0) and then
+             -- If both are true, then we ensure that the index of the last element of the token is
+             --  no greater than the upper bound of the string. Avoids index out-of-bounds error.
+             Tokens(Index).Length-1 <= S'Last - Tokens(Index).Start);
 
 end MyStringTokeniser;
