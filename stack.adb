bbaf51edb038f93340123e975ba855ac04758b5b
chomosuke <a13323600@gmail.com>
2023-05-25 10:07:37 +1000

    fixed stack error

--- stack.adb
+++ stack.adb
@@ -1,15 +1,21 @@
 package body Stack with
    SPARK_Mode
 is
+    procedure Init (St : out Stack) is
+    begin
+        St.Length := 0;
+        St.Content := (others => 0);
+    end Init;
+
     procedure Push (St : in out Stack; I : in Integer) is
     begin
-        St.Content (St.Length) := I;
         St.Length              := St.Length + 1;
+        St.Content (St.Length) := I;
     end Push;
 
-    function Pop (St : in out Stack) return Integer is
+    procedure Pop (St : in out Stack; I : out Integer) is
     begin
+        I := St.Content (St.Length);
         St.Length := St.Length - 1;
-        return St.Content (St.Length + 1);
     end Pop;
 end Stack;
--- stack.ads
+++ stack.ads
@@ -1,21 +1,23 @@
 package Stack with
    SPARK_Mode
 is
-    type StackIndexRange is range 1 .. 512;
-    type StackContent is array (StackIndexRange) of Integer;
+    type StackContent is array (Natural range 1 .. 512) of Integer;
     type Stack is record
         Length  : Natural;
         Content : StackContent;
     end record;
 
+    procedure Init (St : out Stack) with
+       Post => St.Length = 0;
+
     procedure Push (St : in out Stack; I : in Integer) with
        Pre  => St.Length < 512,
        Post =>
-        St.Length = St'Old.Length + 1 and St.Content (St'Old.Length) = I;
+        St.Length = St'Old.Length + 1 and St.Content (St.Length) = I;
 
-    function Pop (St : in out Stack) return Integer with
-       Pre  => St.Length > 1,
+    procedure Pop (St : in out Stack; I : out Integer) with
+       Pre  => St.Length > 1 and St.Length <= 512,
        Post =>
         St.Length = St'Old.Length - 1 and St.Content = St'Old.Content and
-        Pop'Result = St.Content (St'Old.Length);
+        I = St.Content (St'Old.Length);
 end Stack;
