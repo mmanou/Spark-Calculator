dcdc6cbe5e83d6efd355bc225bea6dc9776a2be5
Michael Manoussakis <mihalis.mana@gmail.com>
2023-05-26 23:23:57 +1000

    Revert boilerplate formatting

--- mycommandline.ads
+++ mycommandline.ads
@@ -1,10 +1,10 @@
 package MyCommandLine with SPARK_Mode is
 
-    function Argument_Count return Natural;
+   function Argument_Count return Natural;
 
-    function Command_Name return String;
+   function Command_Name return String;
 
-    function Argument(Number : in Positive) return String with
-      Pre => Number <= Argument_Count;
+   function Argument(Number : in Positive) return String with
+     Pre => Number <= Argument_Count;
 
 end MyCommandLine;
